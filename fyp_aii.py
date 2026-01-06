589b756f3566b96fa33365b6ae04e0b211e4f3f7
safiyyah03 <safiyyahsulaiman1601@gmail.com>
2026-01-07 00:13:10 +0800

    Update fyp_aii.py

--- fyp_aii.py
+++ fyp_aii.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1-md-7riET0fDZS4WXmTBBryiMuMkrrb6
 """
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 
